480da9a59be2fd3b5a55ec6900634a1200dd30c8
montesclarosglennbenedict <montesclarosglennbenedict@gmail.com>
2023-06-20 18:41:59 -0700

    banger setup, now it's on PyPi

---
+++ README.md
@@ -0,0 +1,23 @@
+# ASSIST API Wrapper
+
+[![image](https://img.shields.io/pypi/v/assistorg-api.svg)](https://pypi.python.org/pypi/assistorg-api/)
+
+[![image](https://img.shields.io/travis/montesclarosglennbenedict/assistorg-api.svg)](https://travis-ci.com/montesclarosglennbenedict/assistorg-api)
+
+[![Documentation Status](https://readthedocs.org/projects/assistorg-api/badge/?version=latest)](https://assistorg-api.readthedocs.io/en/latest/?version=latest)
+
+Unofficial API wrapper for ASSIST.org\'s API. UNRELEASED.
+
+-   Free software: MIT license
+-   Documentation: <https://assistorg-api.readthedocs.io>.
+
+## Features
+
+-   TODO
+
+## Credits
+
+This package was created with
+[Cookiecutter](https://github.com/audreyr/cookiecutter) and the
+[audreyr/cookiecutter-pypackage](https://github.com/audreyr/cookiecutter-pypackage)
+project template.
--- setup.py
+++ setup.py
@@ -4,20 +4,29 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md', 'r', encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.md') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
+    name='assistorg-api',
+    version='0.0.2',
+    description="Unofficial API wrapper for ASSIST.org's API.",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     author="Glenn Benedict Montesclaros",
     author_email='montesclarosglennbenedict@gmail.com',
+    url='https://github.com/montesclarosglennbenedict/assist_api_wrapper',
+    packages=find_packages(include=['assist_api_wrapper', 'assist_api_wrapper.*']),
     python_requires='>=3.6',
+    install_requires=requirements,
+    tests_require=test_requirements,
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -28,17 +37,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    description="Unofficial API wrapper for ASSIST.org's API.",
-    install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    keywords='assist_api_wrapper',
-    name='assist_api_wrapper',
-    packages=find_packages(include=['assist_api_wrapper', 'assist_api_wrapper.*']),
-    test_suite='tests',
-    tests_require=test_requirements,
-    url='https://github.com/montesclarosglennbenedict/assist_api_wrapper',
-    version='0.0.01',
     zip_safe=False,
 )
