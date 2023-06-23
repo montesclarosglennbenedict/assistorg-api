a74f4128b44165cb31f711a0c48199e7430f32c7
Replit user <>
2023-06-23 23:17:49 +0000

    Added to the __init.py

--- assist_api_wrapper/__init__.py
+++ assist_api_wrapper/__init__.py
@@ -3,3 +3,9 @@
 __author__ = """Glenn Benedict Montesclaros"""
 __email__ = 'montesclarosglennbenedict@gmail.com'
 __version__ = '0.0.01'
+
+from .academic_years import get_academic_years
+from .agreements import get_agremeents, get_agreements_categories
+from .institutions import get_institutions, get_institutions_academic_years
+from .settings import get_assist_settings
+from .transferability import get_transferability_courses, get_transferability_categories
